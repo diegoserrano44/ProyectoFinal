175ea02d7f7febd2db74ffd8d45614a1613ba1d6
axcpDev <axcpdev@gmail.com>
2022-03-24 09:24:33 +0100

    Vistas

---
+++ app/vistas/layout.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>HELProgramming</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">       
+<script src="https://cdn.tiny.cloud/1/vocxbael83ynt7vuj9xksifglo6tuwy1ik1zunrl6hsflrg0/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@600&family=Press+Start+2P&display=swap" rel="stylesheet">
+
+<link href="css/styles.css" rel="stylesheet">
+<link href="css/boton.css" rel="stylesheet">
+</head>
+
+<body class="bg-dark text-white">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand navTitle" href="index.php">HELP</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <?php if (isset($_GET['ctl'])) {
+                    if((($_GET['ctl'] != 'inicio') || ($_GET['ctl'] == ""))){?>
+                <form class="d-flex" method="POST" action="index.php?ctl=buscar">
+                <input class="navSearch form-control me-2 rounded-pill bg-dark border text-light border-secondary "  name="buscador" type="search" placeholder="Search ..." aria-label="Search">
+                <button class="btn btn-outline-success rounded-circle" type="submit" name="buscar" value="Search"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </form>
+                <?php }};?>
+                </div>
+                <div class="d-lg-flex align-items-center">
+                <?php 
+                if (isset($_SESSION['id_usuario'])) {
+                    echo "<a class=\"nav-link nav-item text-white\" href=\"index.php?ctl=listarAnuncios\">Lessons</a>";
+                    echo "<a class=\"nav-link nav-item text-white\" href=\"index.php?ctl=crearAnuncio\">New ad</a>";
+                    echo "<a class=\"nav-link nav-item text-white\" href=\"index.php?ctl=listarMensaje\">Messages</a>";
+                    echo "<a class=\"nav-link nav-item text-white\" href=\"index.php?ctl=verPerfil&id=" . $_SESSION['id_usuario'] . "\">Profile</a>";
+                    echo "<a class=\"nav-link nav-item text-white\" href=\"index.php?ctl=salirUsuario\"><i class=\"fas fa-sign-out-alt\"></i></a>";
+                }else{
+                    echo "<a class=\"nav-link nav-item text-white\" href=\"index.php?ctl=listarAnuncios\">List lessons</a>";
+                    echo "<a class=\"nav-link nav-item bg-light textSecundario rounded-pill\" href=\"index.php?ctl=entrarUsuario\"><i class=\"fas fa-user-circle\"></i> Login</a>";
+                    echo "<a class=\"nav-link nav-item text-white\" href=\"index.php?ctl=registrarUsuario\">Sign up</a>";
+                }
+                ?>
+                </div>
+            </div>
+        </div>
+    </nav>
+<div class="text-black" id="contenido">
+
+<?php echo $contenido ?>
+
+</div>
+<footer class="bg-dark text-white d-flex flex-column-reverse flex-md-row p-4 w-100 align-items-center justify-content-between">
+            <div>
+                <ul class="list-group list-group-flush">
+                    <h3>HELP Team</h3>
+                    <li class="list-group-item listaFooter">Maria Santana</li>
+                    <li class="list-group-item listaFooter">Diego Serrano</li>
+                    <li class="list-group-item listaFooter">Eduard Romeu</li>
+                    <li class="list-group-item listaFooter">Irene Nebot</li>
+                </ul>
+            </div>
+            <div class="p-4">
+                <a class="text-white m-3 text-decoration-none" href="">IES Abastos - 2022 - HELProgramming</a>
+            </div>
+            <div class="d-flex flex-column justify-content-center">
+            <div>
+                <a href="#"><i class="fa-3x fa-brands fa-instagram m-3 textSecundario"></i></a>
+                <a href="#"><i class="fa-3x fa-brands fa-linkedin m-3 textSecundario"></i></a>
+                <a href="#"><i class="fa-3x fa-brands fa-youtube m-3 textSecundario"></i></a>
+            </div>
+
+        <?php        
+        if (isset($_SESSION['rol'])) {
+            if ($_SESSION['rol']==1) {
+                echo "<button type=\"button\" class=\"btn secundarioDark m-3\" data-bs-toggle=\"modal\" data-bs-target=\"#contactAdmin\">Message to admin</button>";
+            };
+        }?>
+               
+            </div>
+        </div>
+</footer>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+<div class="modal fade" id="contactAdmin" tabindex="-1" aria-labelledby="contactAdminLabel" aria-hidden="true">
+  <div class="modal-dialog" width="fit-content">
+    <div class="modal-content text-dark">
+      <div class="modal-header">
+        <h5 class="modal-title" id="contactAdminLabel">Send message</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form class=" " action="index.php?ctl=mandarMensaje&id=1" method="POST">
+        <input class="asuntoMensaje" type="text" name="asunto" placeholder="Subject"><br><br>
+        <textarea class="contenidoMensaje" rows="6" placeholder="Message" name="mensaje"></textarea>
+      </div>
+      <div class="modal-footer">
+        <input type="submit" name="enviaContacto" value="Send message" class="btn colorSecundario">
+        <a type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</a>
+      </form>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ app/vistas/vInicio.php
@@ -0,0 +1,107 @@
+<?php ob_start();
+if (isset($_SESSION['mensaje'])) {
+    echo "<h2 class='display-6'>" . $_SESSION['mensaje'] . "</h2>";
+}
+if (isset($params['mensaje'])) {
+    echo "<h2 class='display-6'>" . $params['mensaje'] . "</h2>";
+}
+if (! empty($errores)) {
+    foreach ($errores as $key=>$contenido){
+        echo "<p>".$contenido."</p>";
+    }
+
+}
+
+?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+<script src="./../app/ajax/tinyinicio.js"></script>
+
+<div class="jumbotrons jumbotron p-5 bg-dark text-white" style="width:100%;">
+         <div class="container-fluid py-5">
+            <div class="container">
+               <h1 class="display-5 fw-bold text-center">Welcome to Happy Ever Learning Programming</h1>
+               <p class="col-md-12 fs-5 text-center">Search your announcement or place yours</p>
+               <form method="POST" action="index.php?ctl=buscar" class="form-buscador d-flex justify-content-center p-2">
+                    <label for="buscador"></label>
+                    <input type="text" id="buscador" name="buscador" placeholder="Search your class" required>
+                    <button type="submit" class="boton-buscador" name="buscar" value="Search"><i class="fa fa-search"></i></button>
+               </form>
+            </div>
+         </div>
+      </div>
+
+<section style="background-color: #2b2d30;">
+<div class="col-12 text-center p-4 text-white">
+    <h2>Why learning a programming language?</h2>
+</div>
+
+<div class="container mb-4 mt-4 text-white">
+  <div class="row d-flex">
+    <div class="col-md-4 col-sm-12 col-xs-12">
+      <img class="gifindex" src="../app/index.gif" width="100%" height="100%">
+    </div>
+    <div class="col-md-8 col-sm-12 col-xs-12 d-flex align-items-center">
+        <p>Why learn just one programming language when there are so many available to you? Be ambitious and sign
+        up for classes in all the languages you want to learn, with the best teachers and online assistance from
+        Happy Ever Learning Programming.
+        Why learn just one programming language when there are so many available to you? Be ambitious and sign
+        up for classes in all the languages you want to learn, with the best teachers and online assistance from
+        Happy Ever Learning Programming.
+        </p>
+    </div>
+  </div>
+</div>
+
+<div class="col-md-12 p-4 text-white">
+<h2 class="text-center">Most popular languages</h2>
+<div class="d-flex justify-content-center">
+    <canvas id="myChart" style="width:100%;max-width:700px"></canvas>
+</div>
+</div>
+</section>
+
+<div class="jumbotron p-5 bg-dark text-white">
+        <div class="container-fluid py-5">
+            <div class="row d-flex align-items-center">
+              <div class="col-md-6 col-xs-12">
+               <h2 class="display-5 fw-bold">Sign Up<br> and Start <br>your classes now!</h2><br>
+               <a type="button" href="index.php?ctl=registrarUsuario" class="btn btn-light">Sign Up</a>
+              </div>
+              <div class="col-md-6 col-xs-12 pt-4">
+               <textarea id="inicio">
+                 <h2>START OFFERING YOUR PRIVATE LESSONS</h2>
+                 <p>Sign up and share your skills with others in need.</p>
+                 <p>You can create an ad about your skills and share it with others, or if you want to search for private lessons, you can do it! Use the search bar to do so!</p>
+                 <p>Create your profile and start <b>right now!</b></p>
+               </textarea>
+              </div>
+            </div>
+      </div>
+</div>
+
+<section class="text-white p-4 colorSecundario">
+  <div class="row col-md-12 col-sm-12 p-4">
+    <div class="row col-md-6 col-sm-12 col-xs-12">
+        <h3 class="d-flex justify-content-center">What we have to offer?</h3>
+        <p class="text-center">Our platform is suitable for any user<br>
+        Take part in private lessons with the best teachers<br>
+        Programming teachers in the area<br><br>We value our customers, that's why we want to guarantee the best service.<br>
+        +++
+        </p><br><br><br>
+    </div>
+
+<div class="col-md-6 col-sm-12 col-xs-12">
+<ul class="list-group list-group bg-dark">
+  <li class="list-group-item list-group-item-dark"><i class="bi bi-star-fill"></i> Private Teachers</li>
+  <li class="list-group-item list-group-item-dark"><i class="bi bi-star-fill"></i> Online Lessons</li>
+  <li class="list-group-item list-group-item-dark"><i class="bi bi-star-fill"></i> Qualified professionals</li>
+  <li class="list-group-item list-group-item-dark"><i class="bi bi-star-fill"></i> Teacher-student contact</li>
+  <li class="list-group-item list-group-item-dark"><i class="bi bi-star-fill"></i> Best Ratings</li>
+</ul>
+</div>
+</div>
+</section>
+
+<script src="../app/ajax/grafico.js"></script>
+<?php $contenido = ob_get_clean() ?>
+<?php include __DIR__ . './../templates/layout.php' ?>
