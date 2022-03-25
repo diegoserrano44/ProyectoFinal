84f7f2207bc3f14b16c7ae875cf9bfb78831b465
axcpDev <axcpdev@gmail.com>
2022-03-25 08:35:39 +0100

    vistas

---
+++ app/vistas/vLogin.php
@@ -0,0 +1,65 @@
+<?php ob_start() ?>
+
+<main class="container-fluid mainContainer">
+	<div class="container bg-light border-2 border-dark rounded-3 pt-4 pb-5 px-5 shadow loginContainer">
+		<h1 class="text-center fw-bold mt-3 mb-4">Welcome</h1>
+		<?php
+		if (!empty($_SESSION['mensaje'])) {
+			echo "<h4 class='errorMsg'>" . $_SESSION['mensaje'] . "</h4>";
+		}
+		if (!empty($params['mensaje'])) {
+			echo "<h4 class='errorMsg'>" . $params['mensaje'] . "</h4>";
+		}
+		if (!empty($errores)) {
+			foreach ($errores as $key => $contenido) {
+				echo "<p>" . $contenido . "</p>";
+			}
+		}
+		?>
+		<form class="needs-validation" method="POST" action="index.php?ctl=entrarUsuario" novalidate>
+			<div class="has-validation mt-3 mb-4">
+				<label for="Luser" class="form-label fw-bold mb-3 text-dark">Username</label>
+				<input type="text" class="form-control" name="Luser" id="Luser" <?php echo (isset($params['usuario'])) ? "value='" . $params['usuario'] . "'" : "" ?> required>
+				<div class="invalid-feedback">
+					Please provide a username.
+				</div>
+			</div>
+			<div class="has-validation mt-3 mb-4">
+				<label for="Lpass" class="form-label fw-bold mb-3 text-dark">Password</label>
+				<input type="password" class="form-control" name="Lpass" id="Lpass" required>
+				<div class="invalid-feedback">
+					Please provide a password.
+				</div>
+			</div>
+			<button class="btn my-3 w-100 py-2 fw-bold botonAzul" type="submit" name="login">Login</button>		
+			<?php if($login_button != ""){echo $login_button; }?>	
+		</form>
+			<a href="index.php?ctl=enviarPass" >I forgot my password, send me a new one</a>
+	</div>
+</main>
+
+<script>
+	// Example starter JavaScript for disabling form submissions if there are invalid fields
+	(function() {
+		'use strict'
+
+		// Fetch all the forms we want to apply custom Bootstrap validation styles to
+		var forms = document.querySelectorAll('.needs-validation')
+
+		// Loop over them and prevent submission
+		Array.prototype.slice.call(forms)
+			.forEach(function(form) {
+				form.addEventListener('submit', function(event) {
+					if (!form.checkValidity()) {
+						event.preventDefault()
+						event.stopPropagation()
+					}
+
+					form.classList.add('was-validated')
+				}, false)
+			})
+	})()
+</script>
+<?php $contenido = ob_get_clean() ?>
+
+<?php include __DIR__ . './layout.php' ?>
---
+++ app/vistas/vRegistro.php
@@ -0,0 +1,153 @@
+<?php ob_start() ?>
+
+<main class="container-fluid mainContainer">
+    <div class="container bg-light border-2 border-dark rounded-3 pt-4 pb-5 px-5 shadow registroContainer">
+        <h1 class="text-center fw-bold mt-3 mb-4">Sign up</h1>
+        <?php
+        if (!empty($params['mensaje'])) {
+            echo "<h4 class='errorMsg'>" . $params['mensaje'] . "</h4>";
+            if (!empty($params['errores'])) {
+                echo "<ul>";
+                foreach ($errores as $key => $contenido) {
+                    echo "<li>" . $contenido . "</li>";
+                }
+                echo "</ul>";
+            }
+        }
+        ?>
+        <form class="needs-validation" method="POST" action="index.php?ctl=registrarUsuario" novalidate>
+            <div class="container-fluid">
+                <div class="row row-cols-lg-3 has-validation mt-3 mb-4">
+                    <div class="col-lg-auto">
+                        <span class="d-inline-block fw-bold mb-3">Name</span>
+                    </div>
+                    <div class="col-lg-4">
+                        <input type="text" class="form-control" name="nombre" id="nombre" placeholder="First name" required>
+                        <div class="invalid-feedback">
+                            Please provide a name.
+                        </div>
+                    </div>
+                    <div class="col-lg-5">
+                        <input type="text" class="form-control" name="apellidos" id="apellidos" placeholder="Last name">
+                        <div class="valid-feedback">
+                            Looks good!
+                        </div>
+                    </div>
+                </div>
+                <div class="row row-cols-lg-3 has-validation mt-3 mb-4 justify-content-start">
+                    <div class="col-lg-auto">
+                        <span class="d-inline-block form-label fw-bold mb-3">Birthday</span>
+                    </div>
+                    <div class="col-lg-9">
+                        <input type="date" class="form-control" name="fnacimiento" id="fnacimiento" required>
+                        <div class="invalid-feedback">
+                            Please provide your date of birth.
+                        </div>
+                    </div>
+                </div>
+                <div class="row row-cols-lg-3 has-validation mt-3 mb-4 justify-content-start">
+                    <div class="col-lg-auto">
+                        <span class="d-inline-block form-label fw-bold mb-3">Contact info</span>
+                    </div>
+                    <div class="col-lg-4">
+                        <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
+                        <div class="invalid-feedback">
+                            Please provide a valid email.
+                        </div>
+                    </div>
+                    <div class="col-lg-5">
+                        <input type="tel" class="form-control" name="tel" id="tel" placeholder="Phone number">
+                        <div class="valid-feedback">
+                            Looks good!
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="row row-cols-lg-3 has-validation mt-3 mb-1 justify-content-start">
+                    <div class="col-lg-auto">
+                        <span class="d-inline-block form-label fw-bold mb-3">Username</span>
+                    </div>
+                    <div class="col-lg-9">
+                        <input type="text" class="form-control" name="usuario" id="usuario" required  onkeyup="filtra(this.value)">
+                        <div class="invalid-feedback">
+                            Please provide a username.
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="mt-1" id="resultado"></div>
+                <div class="row row-cols-lg-3 has-validation mt-3 mb-4 justify-content-start">
+                    <div class="col-lg-auto">
+                        <span class="d-inline-block form-label fw-bold mb-3">Password</span>
+                    </div>
+                    <div class="col-lg-4">
+                        <input type="password" class="form-control" name="password" id="password" required>
+                        <div class="invalid-feedback">
+                            Please provide a password.
+                        </div>
+                    </div>
+                    <div class="col-lg-5">
+                        <input type="password" class="form-control" name="repite_password" id="repite_password" placeholder="Retype password" required>
+                        <div class="invalid-feedback">
+                            Please provide a password.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="btn my-3 w-100 py-2 fw-bold botonAzul" type="submit" name="sign_up">Sign up</button>
+        </form>
+    </div>
+</main>
+
+<script>
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function() {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+            .forEach(function(form) {
+                form.addEventListener('submit', function(event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+    })()
+    
+            function filtra(str) {
+
+            if (str.length == 0) {
+                //document.getElementById("resultado").innerHTML = "bien";
+                return;
+            } else {
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                    		ms= JSON.parse(this.responseText);
+                        if(ms !== false){                        
+                         	document.getElementById("resultado").innerHTML = "This user already exists.";
+                            document.getElementById("resultado").style.color="red";
+                            document.getElementById("usuario").style.backgroundColor="pink";
+
+                        }else{
+                            document.getElementById("resultado").innerHTML = "";
+                            document.getElementById("usuario").style.backgroundColor="white";
+                        }
+                    }
+                };
+                xmlhttp.open("GET", "index.php?ctl=existeUsuario&id=" + str, true);
+                xmlhttp.send();
+            }
+        }
+        
+        
+</script>
+<?php $contenido = ob_get_clean() ?>
+<?php include __DIR__ . './layout.php' ?>
